5476dcdb484320fa999f3fd2d332ac6726585417
Sean Hyde <hydes2@rpi.edu>
2025-01-21 16:39:23 -0500

    Slight FAQ changes.

--- components/faq/faq.tsx
+++ components/faq/faq.tsx
@@ -10,55 +10,47 @@ type FAQ = {
 const faqs: FAQ[] = [
 	{
 		title: "What is HackRPI?",
-		content:
-			"Teams of 1-4 have 24 hours to build a project relating to our theme Urban Upgrades. Teams will then present their projects, and the best projects win prizes!",
+		content: "come and code.",
 	},
 	{
 		title: "When is HackRPI?",
-		content:
-			"HackRPI takes place on November 9th and 10th, 2024. Arrival and check-in takes place from 9-10 AM. Our opening ceremony starts at 10 AM, and hacking begins at 11am. Your projects must be on Devpost by 10 AM Sunday, and all coding must stop at 11 AM Sunday. Teams will present, and the event will end around 4PM on Sunday. We are excited to see you there!",
+		content: "Never again.",
 	},
 	{
 		title: "Where is HackRPI?",
-		content:
-			"HackRPI takes place at Rensselaer Polytechnic Institute, in the Darrin Communication Center. Darrin Communications Center, 51 College Ave, Troy, NY 12180. See our event information page for more details. ",
+		content: "RPI where else. ",
 	},
 	{
 		title: "Is HackRPI free to attend?",
-		content: "Yes! Thanks to our many wonderful sponsors, all food and swag are completely free for participants!",
+		content: "Nope. Now it's 3 trillion dollars. Good luck.",
 	},
 	{
 		title: "How do I register?",
 		content: (
 			<div className="flex items-start md:items-center justify-start flex-col md:flex-row">
-				<p className="pr-2">You can click here to register with Major League Hacking (MLH).</p>
+				<p className="pr-2">Dont bother. You wont win.</p>
 				<RegistrationButton /> {/* This is a custom button component will update later */}
 			</div>
 		),
 	},
 	{
 		title: "Who can participate?",
-		content:
-			"Everyone is welcome to participate, whether you are a hackathon veteran or this is your first hackathon, this is a great opportunity to learn, experiment with new technologies, and grow your skills. Our team of experienced mentors is here to help you in every step of the way.",
+		content: "Only RPI students.",
 	},
 	{
 		title: "I'm under 18, can I still participate?",
-		content:
-			"Students under 18 are welcome to attend, but are not allowed to stay overnight in the sleep rooms. Students under the age of 17 must have an adult (21+) chaperone with them at all times during the event.",
+		content: "Nope. Minimum age is now 30.",
 	},
 	{
 		title: "Do I have to be an RPI student?",
-		content:
-			"No! HackRPI is open to everyone, students of all experience levels from all colleges and universities are welcome to attend. Did you know that students from over 45 other colleges attended HackRPI!",
+		content: "yes you do.",
 	},
 	{
 		title: "Does HackRPI provide travel reimbursement?",
 		content: (
 			<p>
-				Unfortunately, we are unable to provide travel reimbursement at this time, however, we have sleep rooms on
-				campus for students 18 and older, and we are more than happy to recommend local accommodations if you email us
-				at{" "}
-				<a href="mailto:hackrpi@rpi.edu" className="text-hackrpi-primary-blue underline">
+				What do you think we're rich?{" "}
+				<a href="mailto:dontbother@rpi.edu" className="text-hackrpi-primary-blue underline">
 					hackrpi@rpi.edu
 				</a>
 				.
@@ -67,33 +59,29 @@ const faqs: FAQ[] = [
 	},
 	{
 		title: "What should I bring?",
-		content: "Bring your team, your laptop, chargers, any hardware you need, and a good night's sleep!",
+		content: "your body.",
 	},
 	{
 		title: "What is the theme?",
-		content:
-			"The theme for HackRPI 2024 is Urban Upgrades. This theme is all about enhancing the infrastructure, services, and quality of life in our cities. From smart transportation systems and sustainable energy solutions to improved public safety and inclusive community spaces, this theme challenges you to develop ideas and technologies that make cities more livable, efficient, and enjoyable for everyone.",
+		content: "free choice.",
 	},
 	{
 		title: "Is it okay if I am late to the event?",
-		content:
-			"Yes! You can arrive at any time during the event, but we recommend arriving before 11 AM on Saturday. Remember, the longer you are late, the less time you have to work on your project!",
+		content: "Absolutely not, immediate disqualification.",
 	},
 	{
 		title: "When are submissions due?",
-		content:
-			"All projects must be submitted to Devpost by 10 AM on Sunday. You will be able to modify your submission until 11am. After 11an, no coding or changes to your project are allowed.",
+		content: "2 hours after starting.",
 	},
 	{
 		title: "How do I submit my project?",
 		content:
 			// eslint-disable-next-line
-			'You will submit your project on Devpost. See our "Event Information" and "Resources" pages for more details.',
+			"nah",
 	},
 	{
 		title: "When and how will prizes be awarded?",
-		content:
-			"Prizes are announced at the closing ceremony, which will take place around 4 PM on Sunday. Physical prizes will be distributed during the closing ceremony. Winners of cash prizes will be contacted by our team after the event. ",
+		content: "Whoever has the worst project wins the most money. ",
 	},
 ];
 
