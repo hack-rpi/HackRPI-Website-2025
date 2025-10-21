5f160e2bd98b3e3b9f0c8e73486dbd4c4f5ddada
aarugaut <aaryangaut@gmail.com>
2025-10-21 16:48:34 -0400

    10/21
    I made the registration button work properly and I made the landing page compatible with mobile

--- components/title-components/mobile-title.tsx
+++ components/title-components/mobile-title.tsx
@@ -1,12 +1,21 @@
 "use client";
 
-import TitleText from "./title-text";
+import TitleText from "./mobile-title-text";
+import InteractiveNavigationMap from "../interactive-map/interactive-map";
+import RegistrationButton from "@/components/themed-components/registration-link";
 
 export default function MobileTitleComponent() {
-	return (
-		<div className="w-full h-fit flex flex-col items-start justify-start mb-8 pl-8">
-			<div className="h-28 w-full"></div>
-			<TitleText />
-		</div>
-	);
+  return (
+    <div className="w-full flex flex-col items-center justify-start space-y-6 p-4">
+      {/* Skyline box / main title */}
+      <div className="w-full max-w-[700px]"> 
+        <TitleText />
+      </div>
+
+      {/* Registration button below everything */}
+      <div className="w-full flex justify-center">
+        <RegistrationButton className="text-[28px] px-6 py-3" />
+      </div>
+    </div>
+  );
 }
