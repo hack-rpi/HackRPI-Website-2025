c5768efdf006dbebfc0b1ddfe46605b830ca4d81
iamchwill <iamchwill@gmail.com>
2025-10-24 16:45:03 -0400

    Trying to make the text change

--- components/themed-components/registration-link.tsx
+++ components/themed-components/registration-link.tsx
@@ -4,10 +4,11 @@ export default function RegistrationLink({ className }: { className?: string }) 
 	return (
 		<HackRPILink
 			href="https://hackrpi2024.devpost.com/project-gallery"
-			className={`${className} pl-2 pr-5 py-2`}
+			className={`${className}  px-4 py-2`}
 			target="_blank"
 		>
-			Register Here!
+			<span className="hidden [@media(max-width:500px)]:inline">Register!</span>
+      		<span className="inline [max-width:500px]:hidden">Register Here!</span>
 		</HackRPILink>
 	);
 }
