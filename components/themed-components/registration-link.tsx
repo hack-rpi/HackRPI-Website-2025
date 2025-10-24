ef79f0e641afcf969b06be959f43ac6655ad507d
iamchwill <iamchwill@gmail.com>
2025-10-24 19:55:36 -0400

    IT WORKS

--- components/themed-components/registration-link.tsx
+++ components/themed-components/registration-link.tsx
@@ -4,11 +4,10 @@ export default function RegistrationLink({ className }: { className?: string }) 
 	return (
 		<HackRPILink
 			href="https://hackrpi2024.devpost.com/project-gallery"
-			className={`${className}  px-4 py-2`}
+			className={`${className} pl-2 pr-5 py-2`}
 			target="_blank"
 		>
-			<span className="hidden [@media(max-width:500px)]:inline">Register!</span>
-      		<span className="inline [max-width:500px]:hidden">Register Here!</span>
+			Register Here!
 		</HackRPILink>
 	);
 }
