f0d1f16b5ef744866fd36044662ee012ed9eb386
BastedEggsRYummy <baimej@rpi.edu>
2025-11-01 15:48:45 -0400

    Spacing stuff for the merge conflict

--- app/(with-layout)/last-year/page.tsx
+++ app/(with-layout)/last-year/page.tsx
@@ -10,33 +10,33 @@ import ProgressBar from "@/components/prev-projects/ProgressBar";
 export default function PastYearProjects() {
 	const topProjects = podiumPrizes.slice(0, 3);
 
-	return (
-		<div className="w-full flex items-center justify-center flex-col" id="winners">
-			<ProgressBar />
-			<div className="w-11/12 lg:w-3/4 xl:w-2/3 pt-8 flex flex-col items-center justify-center">
-				<h2 className="text-3xl font-bold text-center mb-8 pt-20">
+  return (
+    <div className="w-full flex items-center justify-center flex-col" id="winners">
+      <ProgressBar />
+      <div className="w-11/12 lg:w-3/4 xl:w-2/3 pt-8 flex flex-col items-center justify-center">
+        <h2 className="text-3xl font-bold text-center mb-8 pt-20">
           Previous Projects From HackRPI XI
         </h2>
-				<HackRPILink href="/last-year#photos" className="text-2xl px-6 py-2 m-2">
-					Jump to Photos
-				</HackRPILink>
-				<div className="grid grid-cols-1 gap-8 mx-auto">
-					{topProjects.map((project, index) => (
-						<div key={index}>
-							<ProjectDisplay {...project} />
-							{index < topProjects.length - 1 && (
-								<hr className="my-8 border-t border-gray-200 w-full desktop:w-1/2 mx-auto" />
-							)}
-						</div>
-					))}
-				</div>
-				<hr className="my-8 border-t border-gray-200 w-full desktop:w-1/2 " />
-				<ProjectCarousel projects={carouselPrizes} />
-				<hr className="my-8 border-t border-gray-200 w-full desktop:w-1/2 " id="photos" />
-				<h2 className="text-3xl font-bold text-center mb-8 pt-24">
+        <HackRPILink href="/last-year#photos" className="text-2xl px-6 py-2 m-2">
+          Jump to Photos
+        </HackRPILink>
+        <div className="grid grid-cols-1 gap-8 mx-auto">
+          {topProjects.map((project, index) => (
+            <div key={index}>
+              <ProjectDisplay {...project} />
+              {index < topProjects.length - 1 && (
+                <hr className="my-8 border-t border-gray-200 w-full desktop:w-1/2 mx-auto" />
+              )}
+            </div>
+          ))}
+        </div>
+        <hr className="my-8 border-t border-gray-200 w-full desktop:w-1/2 " />
+        <ProjectCarousel projects={carouselPrizes} />
+        <hr className="my-8 border-t border-gray-200 w-full desktop:w-1/2 " id="photos" />
+        <h2 className="text-3xl font-bold text-center mb-8 pt-24">
           A Memorable Time in HackRPI XI!
         </h2>
-			</div>
-		</div>
-	);
+      </div>
+    </div>
+  );
 }
