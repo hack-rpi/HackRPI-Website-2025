2322366b66e7376e25dc2320360122f1bd14483f
BastedEggsRYummy <baimej@rpi.edu>
2025-11-01 15:55:13 -0400

    Try try again

--- app/(with-layout)/last-year/page.tsx
+++ app/(with-layout)/last-year/page.tsx
@@ -2,16 +2,20 @@
 
 import React from "react";
 import "@/app/globals.css";
+import NavBar from "@/components/nav-bar/nav-bar";
 import ProjectDisplay from "@/components/prev-projects/project-display";
 import { podiumPrizes, carouselPrizes } from "@/data/previous-prize-winners";
 import ProjectCarousel from "@/components/prev-projects/project-carousel";
 import HackRPILink from "@/components/themed-components/hackrpi-link";
 import ProgressBar from "@/components/prev-projects/ProgressBar";
+import LastYearCollage from "@/components/prev-projects/LastYearCollage";
+
 export default function PastYearProjects() {
-	const topProjects = podiumPrizes.slice(0, 3);
+  const topProjects = podiumPrizes.slice(0, 3);
 
   return (
     <div className="w-full flex items-center justify-center flex-col" id="winners">
+      <NavBar showOnScroll={false} />
       <ProgressBar />
       <div className="w-11/12 lg:w-3/4 xl:w-2/3 pt-8 flex flex-col items-center justify-center">
         <h2 className="text-3xl font-bold text-center mb-8 pt-20">
@@ -36,6 +40,7 @@ export default function PastYearProjects() {
         <h2 className="text-3xl font-bold text-center mb-8 pt-24">
           A Memorable Time in HackRPI XI!
         </h2>
+        <LastYearCollage/>
       </div>
     </div>
   );
